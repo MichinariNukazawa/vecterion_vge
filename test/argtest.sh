4967ad479bb3c229c60ab2f3213aca4b3c0c54a6
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2016-11-28 21:39:40 +0900

    add argtest.sh

--- test/argtest.sh
+++ test/argtest.sh
@@ -19,21 +19,30 @@ rm -rf ${ARGTEST_OUTPUT_BASE}.*
 mkdir -p `dirname ${ARGTEST_OUTPUT_BASE}`
 
 # svg
-./${TARGET} -i ${SOURCE_FILE} -o ${ARGTEST_OUTPUT_BASE}.svg
-[ -s ${ARGTEST_OUTPUT_BASE}.svg ] # file is not zero size
-STR=`file ${ARGTEST_OUTPUT_BASE}.svg` ; [[ "${STR}" =~ "SVG" ]] # file type
+OUTPUT_FILE=${ARGTEST_OUTPUT_BASE}.svg
+./${TARGET} -i ${SOURCE_FILE} -o ${OUTPUT_FILE}
+[ -s ${OUTPUT_FILE} ] # file is not zero size
+STR=`file ${OUTPUT_FILE}` ; [[ "${STR}" =~ "SVG" ]] # file type
+###### content check (tiny. text matching base)
+grep 'width="800.0' ${OUTPUT_FILE} > /dev/null
+grep 'height="800.0' ${OUTPUT_FILE} > /dev/null
+
 # jpg
 ./${TARGET} -i ${SOURCE_FILE} -o ${ARGTEST_OUTPUT_BASE}.jpg
 [ -s ${ARGTEST_OUTPUT_BASE}.jpg ]
 STR=`file ${ARGTEST_OUTPUT_BASE}.jpg` ; [[ "${STR}" =~ "JPEG" ]]
+STR=`file ${ARGTEST_OUTPUT_BASE}.jpg` ; [[ "${STR}" =~ "800x800" ]]
 # png
 ./${TARGET} -i ${SOURCE_FILE} -o ${ARGTEST_OUTPUT_BASE}.png
 [ -s ${ARGTEST_OUTPUT_BASE}.png ]
 STR=`file ${ARGTEST_OUTPUT_BASE}.png` ; [[ "${STR}" =~ "PNG" ]]
+STR=`file ${ARGTEST_OUTPUT_BASE}.png` ; [[ "${STR}" =~ "800 x 800" ]]
+STR=`file ${ARGTEST_OUTPUT_BASE}.png` ; [[ "${STR}" =~ "RGBA" ]]
 # bmp
 ./${TARGET} -i ${SOURCE_FILE} -o ${ARGTEST_OUTPUT_BASE}.bmp
 [ -s ${ARGTEST_OUTPUT_BASE}.bmp ]
 STR=`file ${ARGTEST_OUTPUT_BASE}.bmp` ; [[ "${STR}" =~ "PC bitmap" ]]
+STR=`file ${ARGTEST_OUTPUT_BASE}.bmp` ; [[ "${STR}" =~ "800 x 800" ]]
 
 # jpeg (alias jpg)
 ./${TARGET} -i ${SOURCE_FILE} -o ${ARGTEST_OUTPUT_BASE}.jpeg
