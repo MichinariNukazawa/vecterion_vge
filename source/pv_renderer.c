5082e9d1f5fea531849bbbe76249c2d298d01bb1
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-04-06 23:05:18 +0900

    cleanup PvRenderer.

--- source/pv_renderer.c
+++ source/pv_renderer.c
@@ -7,7 +7,7 @@
 #include "pv_cairo.h"
 
 
-static bool _pv_renderer_is_group_kind(const PvElement *element)
+static bool pv_renderer_is_group_kind_(const PvElement *element)
 {
 	switch(element->kind){
 		case PvElementKind_Root:
@@ -20,7 +20,7 @@ static bool _pv_renderer_is_group_kind(const PvElement *element)
 	}
 }
 
-static bool _pv_renderer_cairo_recursive(
+static bool pv_render_cairo_recursive_(
 		cairo_t *cr,
 		const PvElement *element,
 		const PvRenderOption render_option,
@@ -29,47 +29,44 @@ static bool _pv_renderer_cairo_recursive(
 	bool ret = true;
 	(*level)++;
 
-	if(_pv_renderer_is_group_kind(element)){
-		int num = pv_general_get_parray_num((void **)element->childs);
-		for(int i = 0; i < num; i++){
-			if(!_pv_renderer_cairo_recursive(cr,
+	if(pv_renderer_is_group_kind_(element)){
+		size_t num = pv_general_get_parray_num((void **)element->childs);
+		for(int i = 0; i < (int)num; i++){
+			if(!pv_render_cairo_recursive_(
+						cr,
 						element->childs[i],
 						render_option,
 						level)){
 				pv_error("%d", i);
 				ret = false;
-				goto end;
+				goto failed;
 			}
 		}
 	}else{
 		const PvElementInfo *info = pv_element_get_info_from_kind(element->kind);
-		if(NULL == info){
-			pv_bug("");
-			ret = false;
-			goto end;
-		}
+		pv_assertf(info, "%d", element->kind);
 		if(render_option.render_context.is_focus){
-			if(NULL == info->func_draw_focusing
-					|| !info->func_draw_focusing(cr, render_option, element)){
+			if(!info->func_draw_focusing(cr, render_option, element)){
 				pv_bug("");
 				ret = false;
-				goto end;
+				goto failed;
 			}
 		}else{
-			if(NULL == info->func_draw || !info->func_draw(cr, render_option, element)){
+			if(!info->func_draw(cr, render_option, element)){
 				pv_bug("");
 				ret = false;
-				goto end;
+				goto failed;
 			}
 		}
 	}
 
-end:
+failed:
 	(*level)--;
 	return ret;
 }
 
-static bool _pv_renderer_cairo_background(cairo_t *cr,
+static void pv_rendererd_cairo_background_(
+		cairo_t *cr,
 		const PvVg * const vg,
 		const PvRenderContext render_context)
 {
@@ -108,7 +105,7 @@ static bool _pv_renderer_cairo_background(cairo_t *cr,
 			}
 			break;
 		default:
-			pv_bug("%d", render_context.background_kind);
+			pv_abortf("%d", render_context.background_kind);
 	}
 
 	// frame
@@ -120,8 +117,6 @@ static bool _pv_renderer_cairo_background(cairo_t *cr,
 	}
 
 	cairo_restore(cr); // clear clipping
-
-	return true;
 }
 
 GdkPixbuf *pv_renderer_pixbuf_from_vg(
@@ -130,40 +125,33 @@ GdkPixbuf *pv_renderer_pixbuf_from_vg(
 		const PvFocus *focus,
 		const PvElement *element_overwrite)
 {
-	if(NULL == vg){
-		pv_bug("");
-		return NULL;
-	}
+	pv_assert(vg);
+	GdkPixbuf *pb = NULL;
 
 	const int width  = ceil(vg->rect.w * render_context.scale) + (render_context.margin * 2);
 	const int height = ceil(vg->rect.h * render_context.scale) + (render_context.margin * 2);
 	if(width <= 0 || height <= 0){
-		pv_bug("");
+		pv_bug("%dx%d, %fx%f, %f", width, height, vg->rect.w, vg->rect.h, render_context.scale);
 		return NULL;
 	}
 
 	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
-	if(NULL == surface){
-		pv_bug("");
-		return NULL;
-	}
+	pv_assert(surface);
 
 	cairo_t *cr = cairo_create (surface);
+	pv_assert(cr);
 	cairo_translate(cr, render_context.margin, render_context.margin);
 
-	if(!_pv_renderer_cairo_background(cr, vg, render_context)){
-		pv_error("");
-		return NULL;
-	}
+	pv_rendererd_cairo_background_(cr, vg, render_context);
 
 	PvRenderOption render_option = {
 		.render_context = render_context,
 		.focus = focus,
 	};
 	int level = 0;
-	if(!_pv_renderer_cairo_recursive(cr, vg->element_root, render_option, &level)){
+	if(!pv_render_cairo_recursive_(cr, vg->element_root, render_option, &level)){
 		pv_error("");
-		return NULL;
+		goto failed;
 	}
 
 	if(NULL != focus){
@@ -171,15 +159,16 @@ GdkPixbuf *pv_renderer_pixbuf_from_vg(
 		int num = pv_general_get_parray_num((void **)focus->elements);
 		for(int i = 0; i < num; i++){
 			const PvElement *focus_element = focus->elements[i];
-			if(NULL != focus_element && !_pv_renderer_is_group_kind(focus_element)){
+			if(!pv_renderer_is_group_kind_(focus_element)){
 				render_option.render_context.is_focus = true;
-				if(!_pv_renderer_cairo_recursive(cr, focus_element, render_option, &level)){
+				if(!pv_render_cairo_recursive_(cr, focus_element, render_option, &level)){
 					pv_error("");
-					return NULL;
+					goto failed;
 				}
 			}
 
 			const PvElementInfo *info = pv_element_get_info_from_kind(focus_element->kind);
+			pv_assertf(info, "%d", focus_element->kind);
 			if(0 == i){
 				rect_extent = info->func_get_rect_by_anchor_points(focus_element);
 			}else{
@@ -199,17 +188,16 @@ GdkPixbuf *pv_renderer_pixbuf_from_vg(
 		render_option.focus = NULL;
 		render_option.render_context.is_focus = false;
 		level = 0;
-		if(!_pv_renderer_cairo_recursive(cr, element_overwrite, render_option, &level)){
+		if(!pv_render_cairo_recursive_(cr, element_overwrite, render_option, &level)){
 			pv_error("");
 			return NULL;
 		}
 	}
 
-	GdkPixbuf *pb = gdk_pixbuf_get_from_surface(surface, 0, 0, width, height);
-	if(NULL == pb){
-		pv_error("%d, %d, %f", width, height, render_context.scale);
-		return NULL;
-	}
+	pb = gdk_pixbuf_get_from_surface(surface, 0, 0, width, height);
+	pv_assertf(pb, "%dx%d, %fx%f, %f", width, height, vg->rect.w, vg->rect.h, render_context.scale);
+
+failed:
 
 	cairo_surface_destroy (surface);
 	cairo_destroy (cr);
