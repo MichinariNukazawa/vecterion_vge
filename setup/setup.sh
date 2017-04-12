cc8717ebe44bc6f92190b9e411238aaccf5e906a
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-04-12 22:01:19 +0900

    fix pre-commit hook auto setting setup/setup.sh.

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,8 +9,10 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 
 # install depend package
 #sudo apt-get install gtk3-dev libxml2-dev -y
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+ROOT_DIR=${SCRIPT_DIR}/..
 
 # install git hook
-cp ${ROOT_DIR}/develop/pre-commit ${ROOT_DIR}/.git/hook/pre-commit
-chmod +x ${ROOT_DIR}/.git/hook/pre-commit
+cp ${SCRIPT_DIR}/pre-commit ${ROOT_DIR}/.git/hooks/pre-commit
+chmod +x ${ROOT_DIR}/.git/hooks/pre-commit
 
