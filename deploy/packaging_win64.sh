03cc20c2806bdd46b92efe113da16a4719ef0f80
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-07-28 13:24:38 +0900

    windows package fine run (update to 3.22.15-1-20170610 (unofficial))

--- deploy/packaging_win64.sh
+++ deploy/packaging_win64.sh
@@ -21,35 +21,54 @@ ROOT_DIR=${SCRIPT_DIR}/..
 TARGET_OS=win
 OBJECT_DIR=object/${TARGET_OS}
 CACHE_DIR=${ROOT_DIR}/library/download_cache
-GTK3LIBRARY_DIR=${OBJECT_DIR}/gtk3_win64
+GTK3LIBRARY_DEV_DIR=${OBJECT_DIR}/gtk3_win64/dev
+GTK3LIBRARY_RELEASE_DIR=${OBJECT_DIR}/gtk3_win64/release
 LIBXML2_DIR=${OBJECT_DIR}/libxml2
 BUILD_DIR=build/${TARGET_OS}
 
-#ARCHIVE=gtk+-bundle_3.10.4-20131202_win64.zip
-ARCHIVE=gtk+-bundle_3.22.15-1-20170610_win64.zip
+#ARCHIVE_DEV=gtk+-bundle_3.10.4-20131202_win64.zip
+ARCHIVE_DEV=gtk+-bundle-dev_3.22.15-1-20170610_win64.zip
+ARCHIVE_RELEASE=gtk+-bundle_3.22.15-1-20170610-bin_win64.zip
 
-# download gkt3 library binary
-#if [ ! -e ${CACHE_DIR}/${ARCHIVE} ] ; then
-#	mkdir -p ${CACHE_DIR}
-#	wget --tries=3 --wait=5 --continue \
-#		http://win32builder.gnome.org/gtk+-bundle_3.10.4-20131202_win64.zip \
-#		-P ${CACHE_DIR}
-#fi
+
+# download gkt3 library package
+if [ ! -e ${CACHE_DIR}/${ARCHIVE_DEV} ] ; then
+	mkdir -p ${CACHE_DIR}
+	wget --tries=3 --wait=5 --continue \
+		https://osdn.net/downloads/users/13/13628/gtk%2B-bundle-dev_3.22.15-1-20170610_win64.zip \
+		-O ${CACHE_DIR}/${ARCHIVE_DEV}
+fi
+
+if [ ! -e ${CACHE_DIR}/${ARCHIVE_RELEASE} ] ; then
+	mkdir -p ${CACHE_DIR}
+	wget --tries=3 --wait=5 --continue \
+		https://osdn.net/downloads/users/13/13627/gtk%2B-bundle_3.22.15-1-20170610-bin_win64.zip \
+		-O ${CACHE_DIR}/${ARCHIVE_RELEASE}
+fi
 
 
 # decompress gtk3
-if [ ! -e ${GTK3LIBRARY_DIR}/lib/pkgconfig/gtk+-3.0.pc ] ; then
-	rm -rf ${GTK3LIBRARY_DIR}
-	mkdir -p ${GTK3LIBRARY_DIR}
-	pushd ${GTK3LIBRARY_DIR}
+if [ ! -e ${GTK3LIBRARY_DEV_DIR}/lib/pkgconfig/gtk+-3.0.pc ] ; then
+	rm -rf ${GTK3LIBRARY_DEV_DIR}
+	mkdir -p ${GTK3LIBRARY_DEV_DIR}
+	pushd ${GTK3LIBRARY_DEV_DIR}
 
-	unzip ${CACHE_DIR}/${ARCHIVE} > /dev/null
+	unzip ${CACHE_DIR}/${ARCHIVE_DEV} > /dev/null
 	find -name '*.pc' | while read pc; do sed -e "s@^prefix=.*@prefix=$PWD@" -i "$pc"; done
 	find -name '*.pc' | while read pc; do sed -e "s@/srv/win32builder/fixed_3104/build/win64@$PWD@g" -i "$pc"; done
 
 	popd
 fi
 
+if [ ! -e ${GTK3LIBRARY_RELEASE_DIR}/bin/libgtk-3-0.dll ] ; then
+	rm -rf ${GTK3LIBRARY_RELEASE_DIR}
+	mkdir -p ${GTK3LIBRARY_RELEASE_DIR}
+	pushd ${GTK3LIBRARY_RELEASE_DIR}
+
+	unzip ${CACHE_DIR}/${ARCHIVE_RELEASE} > /dev/null
+
+	popd
+fi
 
 # make resource(icon and other)
 TARGET_OS_WIN_RESOURCE=${OBJECT_DIR}/vecterion.res
@@ -59,8 +78,8 @@ x86_64-w64-mingw32-windres ${ROOT_DIR}/deploy/win/vecterion.rc -O coff -o ${TARG
 # build app source
 CFLAGS_APPEND=-Wno-missing-include-dirs
 
-[ -d ${GTK3LIBRARY_DIR}/lib/pkgconfig ]
-export PKG_CONFIG_PATH=${GTK3LIBRARY_DIR}/lib/pkgconfig
+[ -d ${GTK3LIBRARY_DEV_DIR}/lib/pkgconfig ]
+export PKG_CONFIG_PATH=${GTK3LIBRARY_DEV_DIR}/lib/pkgconfig
 make TARGET_OS=win CC=x86_64-w64-mingw32-gcc CFLAGS_APPEND=${CFLAGS_APPEND} INCLUDE_APPEND="-mwindows" TARGET_OS_WIN_RESOURCE=${TARGET_OS_WIN_RESOURCE}
 
 
@@ -79,21 +98,25 @@ PACKAGE_DIR=${BUILD_DIR}/${PACKAGE_NAME}
 
 rm -rf ${PACKAGE_DIR}
 mkdir -p ${PACKAGE_DIR}
+# vecterion
 cp ${BUILD_DIR}/vecterion_vge.exe ${PACKAGE_DIR}/
 cp -r ${ROOT_DIR}/resource ${PACKAGE_DIR}/
 cp ${ROOT_DIR}/deploy/\!cons.bat ${PACKAGE_DIR}/
 cp ${ROOT_DIR}/deploy/win/README_win_ja.txt ${PACKAGE_DIR}/
-cp ${GTK3LIBRARY_DIR}/bin/*.dll ${PACKAGE_DIR}/
-#cp ${GTK3LIBRARY_DIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll ${PACKAGE_DIR}/
+# library
+cp -r ${GTK3LIBRARY_RELEASE_DIR}/bin/ ${PACKAGE_DIR}/
+# pixbuf loader
 mkdir -p ${PACKAGE_DIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders/
-cp ${GTK3LIBRARY_DIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll ${PACKAGE_DIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders/
+cp -r ${GTK3LIBRARY_RELEASE_DIR}/lib/gdk-pixbuf-2.0/2.10.0/loaders/ ${PACKAGE_DIR}/lib/gdk-pixbuf-2.0/2.10.0/
 cp ${ROOT_DIR}/deploy/win/loaders.cache ${PACKAGE_DIR}/lib/gdk-pixbuf-2.0/2.10.0/
-mkdir -p ${PACKAGE_DIR}/share/glib-2.0/
-cp -r ${GTK3LIBRARY_DIR}/share/glib-2.0/schemas ${PACKAGE_DIR}/share/glib-2.0/
-cp -r ${GTK3LIBRARY_DIR}/share/icons ${PACKAGE_DIR}/share/
+# share(icons, ...etc)
+cp -r ${GTK3LIBRARY_RELEASE_DIR}/share/ ${PACKAGE_DIR}/
+rm -rf ${PACKAGE_DIR}/share/doc
+rm -rf ${PACKAGE_DIR}/share/locale
 
 pushd ${PACKAGE_DIR}
 
+rm -f ${ROOT_DIR}/${BUILD_DIR}/${PACKAGE_NAME}.zip
 zip -r9 ${ROOT_DIR}/${BUILD_DIR}/${PACKAGE_NAME}.zip *
 
 popd
