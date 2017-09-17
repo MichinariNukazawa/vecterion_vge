5f26d488e5e61fef61f83939510cacb58a939e29
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-09-17 15:06:26 +0900

    setup script add mingw-w64

--- setup/library_for_ubuntu.sh
+++ setup/library_for_ubuntu.sh
@@ -8,7 +8,7 @@ trap 'echo "error:$0($LINENO) \"$BASH_COMMAND\" \"$@\""' ERR
 
 
 # install depend package
-sudo apt install libgtk-3-dev libxml2-dev glade librsvg2-bin cmake -y
+sudo apt install libgtk-3-dev libxml2-dev glade librsvg2-bin cmake mingw-w64 -y
 
 
 
